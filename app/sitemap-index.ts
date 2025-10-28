4ada34798939b851bb71669f7113f7b94cf61570
genekellyboyle <DrDuffy@bhhsnv.com>
2025-10-27 21:00:20 -0700

    Update domain from justcalldrjan.com to calldrduffy.com

--- app/sitemap-index.ts
+++ app/sitemap-index.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemapIndex(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.justcalldrjan.com/sitemap.xml',
+      url: 'https://www.calldrduffy.com/sitemap.xml',
       lastModified: new Date(),
     },
   ]
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.justcalldrjan.com'
+  const baseUrl = 'https://www.calldrduffy.com'
 
   // Core pages with highest priority
   const corePages = [
